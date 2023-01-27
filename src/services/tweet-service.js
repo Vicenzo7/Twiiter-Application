ed5e9f58db3500a9f78c3d673a9d115d4e8e380d
Vicenzo7 <deeprajm35@gmail.com>
2023-01-27 11:59:45 +0530

    Tweet service which hashtag proceesing logic

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,24 +1,32 @@
-const TweetRepository = require("../repository/index");
+const { TweetRepository, HashtagRepository } = require("../repository/index");
 
 class TweetService {
   constructor() {
     this.tweetRepository = new TweetRepository();
+    this.hashtagRepository = new HashtagRepository();
   }
 
   async create(data) {
     const content = data.content;
-    const tags = content.match(/#[a-zA-Z0-9_]+/g); // this regex extracts hashtags
-    tags = tags.map((tag) => tag.substring(1));
-    console.log(tags);
+    let tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1)); // this regex extracts hashtags
 
     const tweet = await this.tweetRepository.create(data);
 
-    // todo create hashtags and add here
-    /**
-     * 1. bulk create in mongoose
-     * 2. filter title of hashtag
-     * 3. How to add tweet id inside all the hashtags
-     */
+    let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+
+    let titleOfPresentTags = alreadyPresentTags.map((tags) => tags.title);
+
+    let newTags = tags.filter((tag) => !titleOfPresentTags.includes(tag));
+
+    newTags = newTags.map((tag) => {
+      return { title: tag, tweets: [tweet.id] };
+    });
+
+    await this.hashtagRepository.bulkCreate(newTags);
+    alreadyPresentTags.forEach((tag) => {
+      tag.tweets.push(tweet.id);
+      tag.save();
+    });
     return tweet;
   }
 }
