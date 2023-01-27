bdee28f00946a8dd9866fae6769157468ba5a29f
Vicenzo7 <deeprajm35@gmail.com>
2023-01-27 09:52:13 +0530

    Initial Commit

---
+++ src/models/tweet.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      max: [250, "Tweet cannot be more than 250 characters"],
+    },
+    hashtags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hashtag",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+tweetSchema.virtual("contentWithEmail").get(function process() {
+  return `${this.content} \nCreated by: ${this.userEmail}`;
+});
+
+const Tweet = mongoose.model("Tweet", tweetSchema);
+module.exports = Tweet;
